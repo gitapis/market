a663ba22bc32bd792b96454ca05d4812da69f1b8
Anas esseidi <anas.essaidi@ext.cdiscount.com>
2019-09-18 17:24:58 +0100

    Updates

---
+++ precache-manifest.5808690bec0012e0a9a843fe3af652f8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "289cda261d2a7d3e6e853b30e16d718a",
+    "url": "/market/static/media/AraJozoor-Regular.289cda26.ttf"
+  },
+  {
+    "revision": "081e0913e073f2f9a573",
+    "url": "/market/static/css/main.fe2cf681.chunk.css"
+  },
+  {
+    "revision": "f1413e21498b1474026e",
+    "url": "/market/static/js/runtime~main.f1413e21.js"
+  },
+  {
+    "revision": "7aaaaaa1d1271bba0eca041ce1cf8dbb",
+    "url": "/market/static/media/hp-envy.7aaaaaa1.jpg"
+  },
+  {
+    "revision": "3c9507f0b3fea37312ec",
+    "url": "/market/static/js/2.3c9507f0.chunk.js"
+  },
+  {
+    "revision": "b573015b8452e0f4eb977bbfb193f03f",
+    "url": "/market/static/media/AraJozoor-Regular.b573015b.woff"
+  },
+  {
+    "revision": "f2b8b184ba39f851059aeada97db517b",
+    "url": "/market/static/media/AraJozoor-Regular.f2b8b184.svg"
+  },
+  {
+    "revision": "081e0913e073f2f9a573",
+    "url": "/market/static/js/main.081e0913.chunk.js"
+  },
+  {
+    "revision": "fa1f99dc4c40e5a082e31222a2f2436e",
+    "url": "/market/static/media/AraJozoor-Regular.fa1f99dc.eot"
+  },
+  {
+    "revision": "8225c1b03119e73820f7a277c6777434",
+    "url": "/market/static/media/nikon-d7500.8225c1b0.jpg"
+  },
+  {
+    "revision": "c36402d4130bad81701903c89c0e4416",
+    "url": "/market/static/media/drone-r-falcon.c36402d4.jpg"
+  },
+  {
+    "revision": "4c7d04c8e4ce61008e2ac89ecf15172b",
+    "url": "/market/static/media/hp.4c7d04c8.jpg"
+  },
+  {
+    "revision": "7b2c7410b062a1a7d8a5bde2cef8fda2",
+    "url": "/market/static/media/huawei-p20-lite.7b2c7410.png"
+  },
+  {
+    "revision": "3c9507f0b3fea37312ec",
+    "url": "/market/static/css/2.c9c3b49b.chunk.css"
+  },
+  {
+    "revision": "c3db7dd41f1becd2be9a82ebe1693eb7",
+    "url": "/market/index.html"
+  }
+];
