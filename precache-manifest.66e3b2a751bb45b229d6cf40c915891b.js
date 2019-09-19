6ad99946225ad1ea674d82a0339752703072e657
Anas esseidi <anas.essaidi@ext.cdiscount.com>
2019-09-19 11:09:44 +0100

    Updates

---
+++ precache-manifest.66e3b2a751bb45b229d6cf40c915891b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6249792851aff49f2875ab09cb0bc94b",
+    "url": "/market/index.html"
+  },
+  {
+    "revision": "41e77e2238530c1e49ac",
+    "url": "/market/static/css/2.5db44fde.chunk.css"
+  },
+  {
+    "revision": "99030da8369a8b6ffc7f",
+    "url": "/market/static/css/main.d4d16123.chunk.css"
+  },
+  {
+    "revision": "41e77e2238530c1e49ac",
+    "url": "/market/static/js/2.0cb443f7.chunk.js"
+  },
+  {
+    "revision": "99030da8369a8b6ffc7f",
+    "url": "/market/static/js/main.83a443e0.chunk.js"
+  },
+  {
+    "revision": "502e4c1b6350ba532a77",
+    "url": "/market/static/js/runtime~main.30d04a8e.js"
+  },
+  {
+    "revision": "289cda261d2a7d3e6e853b30e16d718a",
+    "url": "/market/static/media/AraJozoor-Regular.289cda26.ttf"
+  },
+  {
+    "revision": "b573015b8452e0f4eb977bbfb193f03f",
+    "url": "/market/static/media/AraJozoor-Regular.b573015b.woff"
+  },
+  {
+    "revision": "f2b8b184ba39f851059aeada97db517b",
+    "url": "/market/static/media/AraJozoor-Regular.f2b8b184.svg"
+  },
+  {
+    "revision": "fa1f99dc4c40e5a082e31222a2f2436e",
+    "url": "/market/static/media/AraJozoor-Regular.fa1f99dc.eot"
+  },
+  {
+    "revision": "c36402d4130bad81701903c89c0e4416",
+    "url": "/market/static/media/drone-r-falcon.c36402d4.jpg"
+  },
+  {
+    "revision": "7aaaaaa1d1271bba0eca041ce1cf8dbb",
+    "url": "/market/static/media/hp-envy.7aaaaaa1.jpg"
+  },
+  {
+    "revision": "4c7d04c8e4ce61008e2ac89ecf15172b",
+    "url": "/market/static/media/hp.4c7d04c8.jpg"
+  },
+  {
+    "revision": "7b2c7410b062a1a7d8a5bde2cef8fda2",
+    "url": "/market/static/media/huawei-p20-lite.7b2c7410.png"
+  },
+  {
+    "revision": "8225c1b03119e73820f7a277c6777434",
+    "url": "/market/static/media/nikon-d7500.8225c1b0.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/market/precache-manifest.5808690bec0012e0a9a843fe3af652f8.js"
+  "/market/precache-manifest.66e3b2a751bb45b229d6cf40c915891b.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/market/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/market/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
