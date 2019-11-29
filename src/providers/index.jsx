a71541aa2e1f3f590ae080758c14f2227648c0d5
Anas esseidi <anas.essaidi@ext.cdiscount.com>
2019-11-29 16:47:17 +0100

    add React-Admin

--- src/providers/index.jsx
+++ src/providers/index.jsx
@@ -4,12 +4,13 @@ import {isNilOrEmpty} from "../helpers";
 
 const RemoveTokenWithException = (message) => {
     localStorage.removeItem('token');
+    localStorage.removeItem('email');
     throw new Error(message);
 };
 
 const authProvider = {
-    login: ({ username, password }) =>  {
-        return axios.post(`http://boost.ma/api/authenticate`, { 'email' : username, 'password' :password })
+    login: ({ email, password }) =>  {
+        return axios.post(`http://boost.ma/api/authenticate`, { 'email' : email, 'password' :password })
             .then(response => {
                 if(!isNilOrEmpty(response)){
                     if (response.status < 200 || response.status >= 300) {
@@ -29,17 +30,43 @@ const authProvider = {
                     RemoveTokenWithException("No token retrieved");
                 }
                 else {
+                    localStorage.setItem('email', email);
                     localStorage.setItem('token', token);
                 }
             });
     },
     logout: () => {
         localStorage.removeItem('token');
+        localStorage.removeItem('email');
         return Promise.resolve();
     },
-    checkAuth: () => localStorage.getItem('token')
-        ? Promise.resolve()
-        : Promise.reject(),
+    checkAuth: () => {
+        axios.post(`http://boost.ma/api/check`, { 'email' : localStorage.getItem('email'), 'token' :localStorage.getItem('token') })
+            .then(response => {
+                if(!isNilOrEmpty(response)){
+                    if (response.status < 200 || response.status >= 300) {
+                        RemoveTokenWithException(response.statusText);
+                    }
+                    if (isNilOrEmpty(response.data)) {
+                        RemoveTokenWithException("Empty data retrieved");
+                    }
+
+                    return response.data[0];
+                }
+                else {
+                    RemoveTokenWithException("No valid response");
+                }
+            }).then(({isValidUser}) => {
+                if(isNilOrEmpty(isValidUser) || isValidUser === 'undefined'){
+                    RemoveTokenWithException("Invalid properties");
+                }
+                else {
+                    return true
+                }
+            });
+
+        return false
+    },
     checkError: (error) => {
         const status = error.status;
         if (status === 401 || status === 403) {
